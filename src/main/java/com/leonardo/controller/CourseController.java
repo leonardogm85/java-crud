337e3b88b8e355b4d1277424d0316e278b89bc11
Leonardo Martins <leonardogm85@gmail.com>
2023-10-23 22:47:21 -0300

    feat: update course controller

--- src/main/java/com/leonardo/controller/CourseController.java
+++ src/main/java/com/leonardo/controller/CourseController.java
@@ -2,8 +2,12 @@ package com.leonardo.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leonardo.model.Course;
@@ -18,9 +22,20 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
+    // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    // @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // return ResponseEntity
+        //     .status(HttpStatus.CREATED)
+        //     .body(courseRepository.save(course));
+        return courseRepository.save(course);
     }
 
 }
