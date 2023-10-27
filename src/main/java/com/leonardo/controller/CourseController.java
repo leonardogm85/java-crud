d3170d2104a4c4aaa8d585bc5e10c63f84f255b6
Leonardo Gomes Martins <leonardo.gomes@anm.gov.br>
2023-10-26 22:18:15 -0300

    feat: add delete action

--- src/main/java/com/leonardo/controller/CourseController.java
+++ src/main/java/com/leonardo/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,17 @@ public class CourseController {
                     entity.setCategory(course.getCategory());
                     Course updated = courseRepository.save(entity);
                     return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // @RequestMapping(method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(entity -> {
+                    courseRepository.delete(entity);
+                    return ResponseEntity.noContent().<Void>build();
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
