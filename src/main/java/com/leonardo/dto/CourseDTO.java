ee1891b08789ea04cc6e908ccd82e7d90cf76c0a
Leonardo Gomes Martins <leonardo.gomes@anm.gov.br>
2024-02-11 19:50:20 -0300

    feat: validate enums

--- src/main/java/com/leonardo/dto/CourseDTO.java
+++ src/main/java/com/leonardo/dto/CourseDTO.java
@@ -5,16 +5,17 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.leonardo.enums.Category;
+import com.leonardo.enums.validations.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
-                @JsonProperty("_id") Long id,
-                @NotNull @NotBlank @Length(min = 5, max = 100) String name,
-                @NotNull @NotBlank @Length(max = 10) @Pattern(regexp = "backend|frontend") String category,
-                @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
+        @JsonProperty("_id") Long id,
+        @NotNull @NotBlank @Length(min = 5, max = 100) String name,
+        @NotNull @NotBlank @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
+        @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 }
